90e8e0836679be97ca3f4a199a65a99fa6e2050c
Refayat Haque <refayathaque@gmail.com>
2018-11-03 07:54:28 -0400

    staging, commiting and pushing to master

--- prettier.config.js
+++ prettier.config.js
@@ -4,6 +4,6 @@ module.exports = {
     trailingComma: 'all',
     bracketSpacing: true,
     jsxBracketSameLine: false,
-    tabWidth: 4,
+    tabWidth: 2,
     semi: true,
 };
--- src/App.js
+++ src/App.js
@@ -2,45 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let person = {
-    name: 'Yoda',
-    designation: 'Jedi Master ',
-};
-
-function trainJedi(jediWarrion) {
-    if (jediWarrion.name === 'Yoda') {
-        console.log('No need! already trained');
-    }
-    console.log(`Training ${jediWarrion.name} complete`);
-}
-
-trainJedi(person);
-trainJedi({
-    name: 'Adeel',
-    designation: 'padawan',
-});
-
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
