1e0eb16e0c58d72d4c14db56b73e7280d90b052a
Refayat Haque <refayathaque@gmail.com>
2018-11-04 08:00:15 -0500

    Running prettier and linter, and staging, comitting and pushing to branch

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// We get a couple of neat tools bundled with `create-react app`:
+// Webpack - Links together .js files (bundling for deployment, `npm run build`).
+// Babel - Turns ES2015/6/7 and JSX into ES5 code (for older browsers to be able to read).
+// Jest - Automated test runner [ initiated when we do `npm run test` -> finds all files ending with `.test.js` and executes tests inside of them -> prints results to terminal -> waits for any file changes, then runs all tests again ]
+
 // https://medium.com/@Zwenza/functional-vs-class-components-in-react-231e3fbd7108
 // ^ Functional components preferred over class-based because..easier to read and test, end up with less code, helps to use best practices (airbnb es-lint suggests it) and possible performance boost for future React versions
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        I love <code>Théo</code>, my cute boy kitten.
-      </p>
-    </header>
+    <h2>I love Théo, my cute boy kitten.</h2>
   </div>
 );
 
--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,8 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain('<h2>I love Théo, my cute boy kitten.</h2>');
+
   ReactDOM.unmountComponentAtNode(div);
 });
