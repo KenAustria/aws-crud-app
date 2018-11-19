779f78192e24b0220b8effb2b6fece0b35579b62
Refayat Haque <refayathaque@gmail.com>
2018-11-18 19:08:42 -0500

    Running prettier and linter, and staging, comitting and pushing to branch

--- src/actions/comments/__tests__/index.test.js
+++ src/actions/comments/__tests__/index.test.js
@@ -4,10 +4,10 @@ import { SAVE_COMMENT } from 'actions/comments/types';
 describe('saveComment', () => {
   it('Has the correct type', () => {
     const action = saveComment();
-    expect(action.type).toEqual(SAVE_COMMENT)
+    expect(action.type).toEqual(SAVE_COMMENT);
   });
   it('Has the correct payload', () => {
-    const action = saveComment('new comment')
-    expect(action.payload).toEqual('new comment')
+    const action = saveComment('new comment');
+    expect(action.payload).toEqual('new comment');
   });
-})
+});
--- src/reducers/comments/__tests__/comments.test.js
+++ src/reducers/comments/__tests__/comments.test.js
@@ -4,13 +4,13 @@ import { SAVE_COMMENT } from 'actions/comments/types';
 it('Handles actions of type SAVE_COMMENT', () => {
   const action = {
     type: SAVE_COMMENT,
-    payload: 'new comment'
+    payload: 'new comment',
   };
-  const newState = commentsReducer([], action)
+  const newState = commentsReducer([], action);
   expect(newState).toEqual(['new comment']);
 });
 
 it('Handles action with unknown type', () => {
-  const newState = commentsReducer([], { type: 'falskdjfalf' })
+  const newState = commentsReducer([], { type: 'falskdjfalf' });
   expect(newState).toEqual([]);
-})
+});
