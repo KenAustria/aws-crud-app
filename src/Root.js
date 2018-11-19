603fd9bfcc2d2f58fdc58fa1a32440873966f3b9
Refayat Haque <refayathaque@gmail.com>
2018-11-19 09:10:12 -0500

    Running prettier and linter, and staging, comitting and pushing to branch

--- src/Root.js
+++ src/Root.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers/comments';
-import reduxPromise from 'redux-promise'
+import reduxPromise from 'redux-promise';
 
 export default ({ children, initialState = {} }) => {
   // Functional component with ES6 destructuring and default value for initialState
-  const store = createStore(
-    reducers,
-    initialState,
-    applyMiddleware(reduxPromise)
-  )
+  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
   return (
     <Provider store={store}>
       {children}
