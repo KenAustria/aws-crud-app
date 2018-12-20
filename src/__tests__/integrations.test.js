79a9070e31658bedc70ef986b506ed8290c09e28
Refayat Haque <refayathaque@gmail.com>
2018-12-20 13:11:11 +0800

    Running prettier and linter, and staging, comitting and pushing to branch

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -3,39 +3,62 @@ import { mount } from 'enzyme';
 import Root from 'Root';
 import App from 'components/presentation/App';
 import moxios from 'moxios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as actions from 'actions';
+import expect from 'expect';
 
-beforeEach(() => {
-  moxios.install();
-  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
-    status: 200,
-    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }],
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('integration test', () => {
+
+  beforeEach(function () {
+    moxios.install();
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+      status: 200,
+      response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }],
+    });
+    // ^ Moxios intercepting axios' attempted AJAX call and returns an object to axios, fools axios into thinking the object it is getting is from a successful AJAX call
   });
-  // ^ Moxios intercepting axios' attempted AJAX call and returns an object to axios, fools axios into thinking the object it is getting is from a successful AJAX call
-});
 
-afterEach(() => {
-  moxios.uninstall();
-});
+  afterEach(function () {
+    moxios.uninstall();
+  });
 
-it('Entire app can fetch a list of comments and display them', done => {
-  // Attempt to render the *entire* app
-  const wrapped = mount(
-    <Root>
-      <App />
-    </Root>,
-  );
-  // Find the 'fetchComments' button and click it
-  wrapped.find('.fetch-comments').simulate('click');
-  // ^ Will kick off data fetching process
+  it('tests', done => {
+    // Attempt to render the *entire* app
+    const wrapped = mount(
+      <Root>
+        <App />
+      </Root>,
+    );
+    // Find the 'fetchComments' button and click it
+    wrapped.find('.fetch-comments').simulate('click');
+    // ^ Will kick off data fetching process
 
-  // Need to introduce a TINY little pause for moxios to intercept axios call when action creator called, this way the line below will expect whatever is returned by moxios and not axios
-  moxios.wait(() => {
-    wrapped.update();
-    // Expect to find a list of comments!
-    expect(wrapped.find('article')).toHaveLength(2);
-    done();
-    // ^ Jest will consider the test complete only once we invoke the done function
-    wrapped.unmount();
-    // Clean up at the end of a test
+    console.log('helloWorld')
+
+    const expectedActions = [
+      { type: 'FETCH_COMMENTS_LOADING', payload: true },
+      // { type: 'FETCH_COMMENTS_SUCCESS', payload: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }] },
+      // { type: 'FETCH_COMMENTS_LOADING', payload: false },
+    ];
+
+    const store = mockStore({ payload: {} })
+
+    store.dispatch(actions.fetchComments())
+
+    console.log('store.getActions() 1', store.getActions());
+
+    // expect(store.getActions()).toEqual(expectedActions);
+
+    moxios.wait(() => {
+      wrapped.update();
+      store.dispatch(actions.fetchComments())
+      console.log('store.getActions() 2', store.getActions());
+      expect(store.getActions()).toEqual(expectedActions);
+      done();
+    });
   });
 });
